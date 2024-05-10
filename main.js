8d83a28bcfef23070acf80520d470d403f0ddaf6
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-05-10 21:08:47 +0500

    it is a new project

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
-const todoInput = document.querySelector(".todo-input");
-const todoBtn = document.querySelector(".todo-btn");
-const todoList = document.querySelector(".todo-list");
 
-todoBtn.addEventListener("click", addTodo);
+var taskInput = document.getElementById("taskInput");
+var taskList = document.getElementById("taskList");
 
-function addTodo(event) {
-    event.preventDefault();
+function addTask() {
+  var input = taskInput.value.trim();
+  if (input === '') {
+    alert("Please enter a task!");
+    return;
+  }
+  var li = document.createElement("li");
+  li.textContent = input;
+  li.addEventListener("click", toggleTask);
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
 
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
+function toggleTask() {
+  this.classList.toggle("completed");
+}
 
-    const newTodo = document.createElement("li");
-    newTodo.innerHTML = todoInput.value
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-
-    const btnCheck = document.createElement("button");
-    btnCheck.innerHTML = '<i class="fa-solid fa-check"></i>'
-    btnCheck.classList.add("check-btn");
-    todoDiv.appendChild(btnCheck);
-
-    const btnTrash = document.createElement("button");
-    btnTrash.innerHTML = '<i class="fa-solid fa-trash"></i>'
-    btnTrash.classList.add("trash-btn");
-    todoDiv.appendChild(btnTrash);
-    todoList.appendChild(todoDiv);
-    todoDiv.style.display = 'flex';
-    todoDiv.style.justifyContent = 'center'
-    todoDiv.style.alignItems = 'center'
-    todoDiv.style.gap = '10px'
-    todoDiv.style.listStyle = 'none'
-    todoDiv.style.marginTop = '20pxw'
+function deleteCompletedTasks() {
+  var completedTasks = document.querySelectorAll(".completed");
+  completedTasks.forEach(function (task) {
+    task.remove();
+  });
 }
